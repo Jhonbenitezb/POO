3f7f69d66bef6cd4c231b79bb99fb38bd14ddd33
Jhonbenitezb <143809723+Jhonbenitezb@users.noreply.github.com>
2023-10-20 08:32:33 -0500

    Add files via upload

---
+++ Agregacion/agregacion/Agregacion.java
@@ -0,0 +1,30 @@
+
+package agregacion;
+
+import modelos.Departamento;
+import modelos.Empleado;
+
+public class Agregacion {
+
+
+    public static void main(String[] args) {
+          // Crear empleados
+        Empleado empleado1 = new Empleado("Juan");
+        Empleado empleado2 = new Empleado("María");
+
+        // Crear departamento
+        Departamento departamento = new Departamento("Recursos Humanos");
+
+        // Agregar empleados al departamento
+        departamento.agregarEmpleado(empleado1);
+        departamento.agregarEmpleado(empleado2);
+
+        // Mostrar información
+        System.out.println("Departamento: " + departamento.getNombre());
+        System.out.println("Empleados:");
+        for (Empleado empleado : departamento.getEmpleados()) {
+            System.out.println("- " + empleado.getNombre());
+        }
+    }
+    
+}
---
+++ Agregacion/modelos/Empleado.java
@@ -0,0 +1,15 @@
+
+package modelos;
+
+
+public class Empleado {
+    private String nombre;
+
+    public Empleado(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
